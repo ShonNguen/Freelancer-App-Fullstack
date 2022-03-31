2a71630d49fc04589b02950c5cdc4364f2b4ff82
ShonNguen <nguenshon@gmail.com>
2022-03-31 07:03:16 +0300

    REST API for jobs

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -1,29 +1,61 @@
 const asyncHandler = require('express-async-handler');
 
+const Job = require('../models/jobsModel');
+
 
 const getJobs = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get jobs' });
+    const jobs = await Job.find();
+    res.status(200).json(jobs);
 })
 
 const postJob = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
-        res.status(400); 
-        throw new Error('Please add a text field'); 
+    if (!req.body.text) {
+        res.status(400);
+        throw new Error('Please add a text field');
     }
-    res.status(200).json({ message: 'Create jobs' });
+
+    const job = await Job.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(job);
 })
 
 const putJob = asyncHandler(async (req, res) => {
-    res.status(200).json({ message:  `Update job ${req.params.id}`});
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updatedJob = await Job.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true
+        }
+    );
+
+    res.status(200).json(updatedJob);
 })
 
 const deleteJob = asyncHandler(async (req, res) => {
-    res.status(200).json({ message:  `Delete job ${req.params.id}`});
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    await job.remove();
+
+    res.status(200).json({ id: req.params.id });
 })
 
 module.exports = {
     getJobs,
-    postJob, 
-    putJob, 
+    postJob,
+    putJob,
     deleteJob
 }
